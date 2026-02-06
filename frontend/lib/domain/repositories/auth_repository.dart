24d251feb8ba977747cf13a34d5b3ac1b960818e
Musab45 <musabshehzad88@gmail.com>
2026-02-06 23:55:05 +0500

    - domain user service

--- frontend/lib/domain/repositories/auth_repository.dart
+++ frontend/lib/domain/repositories/auth_repository.dart
@@ -1,1 +1,28 @@
-abstract class AuthRepository {}
+import 'package:dartz/dartz.dart';
+import '../../core/errors/failures.dart';
+import '../entities/user.dart';
+
+abstract class AuthRepository {
+  // login with email and password
+  Future<Either<Failure, User>> login({
+    required String email,
+    required String password,
+  });
+
+  // register a new user
+  Future<Either<Failure, User>> register ({
+    required name,
+    required email,
+    required password,
+    required passwordConfirmation,
+  });
+
+  // logout current user
+  Future<Either<Failure, void>> logout();
+
+  // check if logged in
+  Future<bool> isLoggedIn();
+
+  // get current user from storage
+  Future<Either<Failure, User>> getCurrentUser();
+}
